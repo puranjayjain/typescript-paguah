d1df77784d0f54d2d740815be9267599c15a3d33
puranjayjain <puru1joy@gmail.com>
2022-04-22 20:49:54 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
 // Import stylesheets
 import './style.css';
+import moment from 'moment';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+const inputEl: HTMLInputElement = document.getElementById('dateinput');
+const outputEl: HTMLElement = document.getElementById('output');
+
+appDiv.innerHTML = `<h1>Date Time Criteria Format Converter</h1>`;
+
+// Execute a function when the user releases a key on the keyboard
+inputEl.addEventListener('keyup', (event) => {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    outputEl.innerHTML = moment(
+      inputEl.value,
+      'DD/MM/YYYY, hh:mm A'
+    ).toISOString();
+  }
+});
